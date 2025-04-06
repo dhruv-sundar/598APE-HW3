254fdd6efc866d9e95abfe1c3c64e5a7b8572801
ananthm3 <ananthm3@illinois.edu>
2025-04-05 19:38:45 -0500

    Converted AOS to SOA

--- main.cpp
+++ main.cpp
@@ -5,17 +5,43 @@
 
 #include <sys/time.h>
 
+int nplanets;
+int timesteps;
+constexpr double dt = 0.001;
+constexpr double G = 6.6743;
+
+struct PlanetCoords {
+   double *x;
+   double *y;
+   double *vx;
+   double *vy;
+
+   PlanetCoords(int num_planets):
+      x((double *)calloc(num_planets, sizeof(double))), 
+      y((double *)calloc(num_planets, sizeof(double))), 
+      vx((double *)calloc(num_planets, sizeof(double))), 
+      vy((double *)calloc(num_planets, sizeof(double)))
+   {}
+
+   ~PlanetCoords() {
+      free(x);
+      free(y);
+      free(vx);
+      free(vy);
+   }
+
+   PlanetCoords &operator=(const PlanetCoords &other) {
+      std::memcpy(x, other.x, nplanets * sizeof(double));
+      std::memcpy(y, other.y, nplanets * sizeof(double));
+      std::memcpy(vx, other.vx, nplanets * sizeof(double));
+      std::memcpy(vy, other.vy, nplanets * sizeof(double));
+      return *this;
+   }
+};
+
 float tdiff(struct timeval *start, struct timeval *end) {
   return (end->tv_sec-start->tv_sec) + 1e-6*(end->tv_usec-start->tv_usec);
 }
-
-struct Planet {
-   double mass;
-   double x;
-   double y;
-   double vx;
-   double vy;
-};
 
 unsigned long long seed = 100;
 
@@ -35,36 +61,26 @@ double randomDouble()
    return ((next << 27) + next2) / (double)(1LL << 53);
 }
 
-int nplanets;
-int timesteps;
-constexpr double dt = 0.001;
-constexpr double G = 6.6743;
-
-void next(Planet * __restrict__ planets, Planet * __restrict__ nextplanets) {
-   // for (int i=0; i<nplanets; i++) {
-   //    nextplanets[i].vx = planets[i].vx;
-   //    nextplanets[i].vy = planets[i].vy;
-   //    nextplanets[i].mass = planets[i].mass;
-   //    nextplanets[i].x = planets[i].x;
-   //    nextplanets[i].y = planets[i].y;
-   // }
-   std::memcpy(nextplanets, planets, sizeof(Planet) * nplanets);
+void next(const PlanetCoords &planets, PlanetCoords &nextplanets, const double *planet_masses) {
    
+   nextplanets = planets;
+
+   // #pragma omp parallel for schedule(static)
    for (int i=0; i<nplanets; i++) {
       for (int j=0; j<nplanets; j++) {
-         double dx = planets[j].x - planets[i].x;
-         double dy = planets[j].y - planets[i].y;
+         double dx = planets.x[j] - planets.x[i];
+         double dy = planets.y[j] - planets.y[i];
          double distSqr = dx*dx + dy*dy + 0.0001;
-         double invDist = planets[i].mass * planets[j].mass / sqrt(distSqr);
+         double invDist = planet_masses[i] * planet_masses[j] / sqrt(distSqr);
          double invDist3 = invDist * invDist * invDist;
-         nextplanets[i].vx += dt * dx * invDist3;
-         nextplanets[i].vy += dt * dy * invDist3;
+         nextplanets.vx[i] += dt * dx * invDist3;
+         nextplanets.vy[i] += dt * dy * invDist3;
       }
    }
 
    for (int i = 0; i < nplanets; ++i) {
-      nextplanets[i].x += dt * nextplanets[i].vx;
-      nextplanets[i].y += dt * nextplanets[i].vy;
+      nextplanets.x[i] += dt * nextplanets.vx[i];
+      nextplanets.y[i] += dt * nextplanets.vy[i];
    }
 }
 
@@ -76,25 +92,28 @@ int main(int argc, const char** argv){
    nplanets = atoi(argv[1]);
    timesteps = atoi(argv[2]);
 
-   Planet* planets = (Planet*)malloc(sizeof(Planet) * nplanets);
-   Planet* nextplanets = (Planet*)malloc(sizeof(Planet) * nplanets);
+   PlanetCoords planets(nplanets), nextplanets(nplanets);
+   double *planet_masses = (double *)malloc(sizeof(double) * nplanets);
    for (int i=0; i<nplanets; i++) {
-      planets[i].mass = randomDouble() * 10 + 0.2;
-      planets[i].x = ( randomDouble() - 0.5 ) * 100 * pow(1 + nplanets, 0.4);
-      planets[i].y = ( randomDouble() - 0.5 ) * 100 * pow(1 + nplanets, 0.4);
-      planets[i].vx = randomDouble() * 5 - 2.5;
-      planets[i].vy = randomDouble() * 5 - 2.5;
+      planet_masses[i] = randomDouble() * 10 + 0.2;
+      planets.x[i] = ( randomDouble() - 0.5 ) * 100 * pow(1 + nplanets, 0.4);
+      planets.y[i] = ( randomDouble() - 0.5 ) * 100 * pow(1 + nplanets, 0.4);
+      planets.vx[i] = randomDouble() * 5 - 2.5;
+      planets.vy[i] = randomDouble() * 5 - 2.5;
    }
 
    struct timeval start, end;
    gettimeofday(&start, NULL);
-   for (int i=0; i<timesteps; i++) {
-      next(planets, nextplanets);
-      std::swap(planets, nextplanets);
+   for (int i = 0; i < (timesteps & ~0x1); i++) {
+      next(planets, nextplanets, planet_masses);
+      next(nextplanets, planets, planet_masses);
       // printf("x=%f y=%f vx=%f vy=%f\n", planets[nplanets-1].x, planets[nplanets-1].y, planets[nplanets-1].vx, planets[nplanets-1].vy);
    }
+   if (timesteps & 0x1) {
+      next(planets, nextplanets, planet_masses);
+   }
    gettimeofday(&end, NULL);
-   printf("Total time to run simulation %0.6f seconds, final location %f %f\n", tdiff(&start, &end), planets[nplanets-1].x, planets[nplanets-1].y);
+   printf("Total time to run simulation %0.6f seconds, final location %f %f\n", tdiff(&start, &end), nextplanets.x[nplanets-1], nextplanets.y[nplanets-1]);
 
    return 0;   
 }
